e678e45af5ea9dee4e23bb8e9e20ef4e23de7fbe
Sleeeeeepy <adjustedmin@gmail.com>
2023-11-18 10:32:25 +0900

    feat: adder form

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -14,8 +14,8 @@ int main(void) {
         *p = '\0';
         strcpy(arg1, buf);
         strcpy(arg2, p + 1);
-        n1 = atoi(arg1);
-        n2 = atoi(arg2);
+        sscanf(arg1, "num1=%d", &n1);
+        sscanf(arg2, "num2=%d", &n2);
     }
 
     sprintf(content, "QUERY_STRING=%s", buf);
