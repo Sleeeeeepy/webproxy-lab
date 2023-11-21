8f1969c3992199183af3a5f2725d28b2495318d0
Sleeeeeepy <adjustedmin@gmail.com>
2023-11-21 19:14:25 +0900

    feat: fast strstr

--- string.c
+++ string.c
@@ -5,7 +5,7 @@
 
 #include "string.h"
 
-int endsWith(const char *str, const char *suffix) {
+int endsWith(const char* str, const char* suffix) {
     size_t str_len = strlen(str);
     size_t suffix_len = strlen(suffix);
 
@@ -23,4 +23,61 @@ char* strncatf(char* c, size_t n,  char* format, ...) {
     int result = vsnprintf(buf, n, format, args);
     va_end(args);
     return strncat(c, buf, n);
+}
+
+static void fill_lps_arr__(const char* pattern, int n, int* lps) {
+    int len = 0;
+    int i = 1;
+    lps[0] = 0;
+
+    while (i < n) {
+        if (pattern[i] == pattern[len]) {
+            len++;
+            lps[i] = len;
+            i++;
+        } else {
+            if (len != 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+char* fast_strstr(const char* haystack, const char* needle) {
+    size_t needle_len = strlen(needle);
+    size_t haystack_len = strlen(haystack);
+
+    if (needle_len == 0) {
+        return (char *)haystack;
+    }
+
+    int *lps = (int *)malloc(sizeof(int) * needle_len);
+    fill_lps_arr__(needle, needle_len, lps);
+
+    int i = 0;
+    int j = 0;
+
+    while (i < haystack_len) {
+        if (needle[j] == haystack[i]) {
+            j++;
+            i++;
+        }
+
+        if (j == needle_len) {
+            free(lps);
+            return (char *)(haystack + i - j);
+        } else if (i < haystack_len && needle[j] != haystack[i]) {
+            if (j != 0) {
+                j = lps[j - 1];
+            } else {
+                i++;
+            }
+        }
+    }
+
+    free(lps);
+    return NULL;
 }
--- string.h
+++ string.h
@@ -1,2 +1,3 @@
-int endsWith(const char *str, const char *suffix);
+int endsWith(const char* str, const char* suffix);
 char* strncatf(char* c, size_t n,  char* format, ...);
+char* fast_strstr(const char* haystack, const char* needle);
